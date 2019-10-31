5677b97e376cd34417ecb5d27c498679aa2c574e
cocoitali <cmburatto@gmail.com>
2019-10-31 09:55:40 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("Hello from the effect hook!");
+  });
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun!
       </p>
     </div>
   );
